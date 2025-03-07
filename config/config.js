f86a6871c7e1c7ee2f61a4dd5f5c7f22c90d8b03
Eyohen <henry.eyo2@gmail.com>
2025-03-07 12:19:40 +0100

    third commit

--- config/config.js
+++ config/config.js
@@ -6,15 +6,15 @@ module.exports = {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
-    // "host": process.env.DB_HOST,
-    "host": "127.0.0.1",
+    "host": process.env.DB_HOST,
+    //"host": "127.0.0.1",
     "dialect": "postgres",
-    // "dialectOptions": {
-    //   "ssl": {
-    //     "require": true,
-    //     "rejectUnauthorized": false
-    //   }
-    // }
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      },
+    },
   },
   "test": {
     "username": "root",
@@ -29,10 +29,10 @@ module.exports = {
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+    "dialectOptions": {
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false,
       },
     },
   }
